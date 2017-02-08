1e688f9aee7fb9e1f0e90dacf9c84e7cc300603e
helogargon <helogargon@gmail.com>
2017-02-08 14:17:29 +0100

    añadido código java

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,15 @@
-$(function(){
+$(function () {
   
-    $('.button-followers').click(function(){
-                                                 $(this).next('.alert-unFollowUser').css('visibility','inherit');
-                                            }
-                                )    
-})
+    $('.button-followers').click(function () {
+        $(this).next('.alert-unFollowUser').css('visibility', 'inherit');
+    });
+
+    $('.alert-unFollowUser > .btn-danger').click(function () {
+        $(this).parents('.alert-unFollowUser').css('visibility', 'hidden');
+    });
+
+    $('.alert-unFollowUser > .btn-success').click(function () {
+        $(this).parent().next('.alert-unFollowUser').css('visibility', 'hidden');
+        $(this).parents('.panel panel-default').remove();
+    });
+});
