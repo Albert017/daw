390e8a5da1234dc3d7c1600774a3537ac4f49946
Helena García González <helogargon@gmail.com>
2017-02-08 12:41:40 +0100

    archivo javascript con jquery

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+$(function(){
+  
+    $('.button-followers').click(function(){
+                                                 $(this).next('.alert-unFollowUser').css('visibility','inherit');
+                                            }
+                                )    
+})
