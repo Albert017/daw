e12871768349b97ebe1f97648f2500eea6e0622a
helogargon <helogargon@gmail.com>
2017-02-16 10:16:53 +0100

    Añadido código botón seguir a usuario cualquiera

--- js/javascript.js
+++ js/javascript.js
@@ -11,4 +11,14 @@ $(function () {
     $('.button-follow-user').click(function () {
         $(this).remove();
     });
+
+    $('#follow').click(function () {
+        if ($(this).text() === 'Follow') {
+            $(this).text("Following");
+            $(this).css("background-color", "#b9e1ff");
+        } else {
+            $(this).text("Follow");
+            $(this).css("background-color", "#42a7f4");
+        }
+    });
 });
