981132d679525f3a819c08dd1e22a240a47b613b
jjluczyn <kuba.luczyn@gmail.com>
2017-03-12 02:29:12 +0100

    Last merge fix.

--- fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/DatabaseDataLoader.java
+++ fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/DatabaseDataLoader.java
@@ -16,21 +16,15 @@ public class DatabaseDataLoader {
 
 	    @PostConstruct
 	    private void initDatabase() {
-<<<<<<< HEAD
-	    	for(int i=1;i<20;i++){
-	    		User newUs = (new User("user"+i, "pass"+i, "user"+i+".a@a.aa", "ROLE_USER"));
-	    		userRepository.save(newUs);
-=======
 	    	int n=10;
-	    	User newUs [] = new User[n];
+	    	User newUs[] = new User[n];
 	    	for(int i=0;i<n;i++){
-	    		newUs[i] = (new User("user"+i, "pass"+i, "user"+i+".a@a.aa", "USER_ROLE"));
+	    		newUs[i] = (new User("user"+i, "pass"+i, "user"+i+".a@a.aa", "ROLE_USER"));
 	    		userRepository.save(newUs[i]);
 	    		if(i>0){
 	    			Message m1 = new Message("Texto de prueba "+i, newUs[0], newUs[1]);
 	    			messageRepository.save(m1);
 	    		}
->>>>>>> origin/master
 	    	}
 	    	userRepository.save(new User("admin", "adminpass", "admin.a@a.aa","ROLE_USER","ROLE_ADMIN"));
 
