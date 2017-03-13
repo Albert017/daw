025cbaaf50c0068da1605bd739e2ae8a51e5b6b3
jjluczyn <kuba.luczyn@gmail.com>
2017-03-13 13:25:39 +0100

    Fixed User controller for user pages.

--- fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/UsersController.java
+++ fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/UsersController.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import antlr.TokenWithIndex;
@@ -49,8 +50,20 @@ public class UsersController {
 		commentRepository.save(c1);
 
 	}
-	@RequestMapping("/users")
-	public String usersController(Model model){
+	@RequestMapping("/user/{reqUserName}")
+	public String usersController(Model model,
+			@PathVariable String reqUserName){
+		model.addAttribute("loggedUser",userComponent.isLoggedUser());
+		if (userComponent.isLoggedUser()){
+			model.addAttribute("loggedUsername",userComponent.getLoggedUser().getUsername());
+		}
+		User viewUser = userRepository.findByusername(reqUserName);
+		List<Post> postListCurr = viewUser.getUserPosts();
+		Post ranPost = null;
+		if (postListCurr.size()>0){
+			ranPost = postListCurr.get((int)(Math.random()*postListCurr.size()));
+		}
+		model.addAttribute("Posts",ranPost);
 		
 		return "users";
 	}
