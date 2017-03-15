7a92be6ba1622e24cf01bef27cbaa76f17ccf219
Albert017 <alberto_naval94@hotmail.com>
2017-03-15 18:21:55 +0100

    Añadido que muestre la conversacion entre dos usuarios

--- fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/MessageRepository.java
+++ fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/MessageRepository.java
@@ -12,10 +12,10 @@ public interface MessageRepository extends JpaRepository<Message, Long>{
 	List<Message> findByMessageAddresseeAndMessageSenderAndMessageDeletedOrderByIdDesc(User messageAddressee, User messageSender, boolean messageDeleted);
 	List <Message> findByMessageAddresseeAndMessageNew(User user, boolean messageRead);
 	Message findMessageById(Long id);
-	/*
-	@Query("SELECT * FROM MESSAGE m WHERE (m.MESSAGE_ADDRESSEE_ID=?1 AND m.MESSAGE_SENDER_ID=?1) OR (MESSAGE_ADDRESSEE_ID=?1 AND MESSAGE_SENDER_ID=?2)")
-    public List<Message> findConversationById(Long id1);
 	
+	@Query(value="SELECT * FROM MESSAGE  WHERE (MESSAGE_ADDRESSEE_ID=?1 AND MESSAGE_SENDER_ID=?2) OR (MESSAGE_ADDRESSEE_ID=?2 AND MESSAGE_SENDER_ID=?1)  ORDER BY ID DESC", nativeQuery=true)
+    public List<Message> findConversationByUserIdOrderByIdDesc(Long id1, Long id2);
+	/*
 	(Enviado == user1 AND recibido == user2) OR (enviado == user2 AND recibido == user1)
 	
 	
