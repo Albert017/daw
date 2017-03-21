43ff910563a0af629d295c910c06947908b72c7f
jjluczyn <kuba.luczyn@gmail.com>
2017-03-21 09:42:48 +0100

    Revert "Changed to MySQL for tag."
    This reverts commit 3506515a8ea620d3ed50c283aedd8fd26a9cd984.

--- fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/PostRepository.java
+++ fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/PostRepository.java
@@ -12,20 +12,23 @@ public interface PostRepository extends JpaRepository<Post, Long>{
 	List<Post> findAllByOrderByPostUpVotesDesc();
 	List<Post> findAllByreport(boolean report);
 	
+	/**Post findFirstByyearOrderBypostUpVotesAsc(int year);
+	Post findFirstBydayOrderBypostUpVotesAsc(int day);
+	Post findFirstBymonthOrderBypostUpVotesAsc(String month);**/
 	
-	@Query(value = "SELECT * FROM POST WHERE MONTH=?#{[0]} AND YEAR=?#{[1]} ORDER BY POST_UP_VOTES DESC LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM POST WHERE (POST_UP_VOTES =SELECT MAX(POST_UP_VOTES) FROM POST WHERE (MONTH=?#{[0]} AND YEAR=?#{[1]}))", nativeQuery = true)
 	List<Post> findTop1BypostUpVotesMonth(String month, int year);
 	
-	@Query(value = "SELECT * FROM POST WHERE YEAR=?#{[0]} ORDER BY POST_UP_VOTES DESC LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM POST WHERE (POST_UP_VOTES =SELECT MAX(POST_UP_VOTES) FROM POST WHERE YEAR=?#{[0]})", nativeQuery = true)
 	List<Post> findTop1BypostUpVotesYear(int year);
 	
-	@Query(value = "SELECT * FROM POST WHERE MONTH=?#{[0]} AND YEAR=?#{[1]} AND DAY=?#{[2]} ORDER BY POST_UP_VOTES DESC LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM POST WHERE (POST_UP_VOTES =SELECT MAX(POST_UP_VOTES) FROM POST WHERE (MONTH=?#{[0]} AND YEAR=?#{[1]} AND DAY=?#{[2]}))", nativeQuery = true)
 	List<Post> findTop1BypostUpVotesDay(String month, int year, int day);
 	
-	@Query(value = "SELECT * FROM POST WHERE (POST_WEEK=?#{[0]} AND YEAR=?#{[1]}) ORDER BY POST_UP_VOTES DESC LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM POST WHERE (POST_UP_VOTES =SELECT MAX(POST_UP_VOTES) FROM POST WHERE (POST_WEEK=?#{[0]} AND YEAR=?#{[1]}))", nativeQuery = true)
 	List<Post> findTop1BypostUpVotesWeek(int week, int year);
 	
-	@Query(value = "SELECT * FROM POST WHERE (POST_WEEK=?#{[0]} AND YEAR=?#{[1]}) OR (POST_WEEK=?#{[2]} AND YEAR=?#{[3]}) ORDER BY POST_UP_VOTES DESC LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM POST WHERE (POST_UP_VOTES =SELECT MAX(POST_UP_VOTES) FROM POST WHERE (POST_WEEK=?#{[0]} AND YEAR=?#{[1]}) OR (POST_WEEK=?#{[2]} AND YEAR=?#{[3]}))", nativeQuery = true)
 	List<Post> findTop1BypostUpVotesWeek(int week1, int year1, int week2, int year2);
 	
 	Post findBypostTitle(String postTitle);
