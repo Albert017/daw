08af5f76b1597e0e196dfbf77920fab7ad6f41da
jjluczyn <kuba.luczyn@gmail.com>
2017-03-27 12:48:48 +0200

    Added rest controller example for post
    Can check post.
    TODO author of the post creates infinite recursion with
    followers/following.

---
+++ fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/restcontrollers/PostRestController.java
@@ -0,0 +1,31 @@
+package com.filadeatras.fila_de_atras.restcontrollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.filadeatras.fila_de_atras.models.Post;
+import com.filadeatras.fila_de_atras.repositories.PostRepository;
+
+@RestController
+@RequestMapping("/api/post")
+public class PostRestController {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> postController (@PathVariable long id){
+		
+		Post postFound = postRepository.getOne(id);
+		if(postFound==null){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(postFound,HttpStatus.OK);
+	}
+}
