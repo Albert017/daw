0e0fceb51cb340853701e0e2f67c7b8305e8738c
Albert017 <alberto_naval94@hotmail.com>
2017-03-30 23:06:23 +0200

    Añadido mensajes/username pero no funciona

--- fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/restcontrollers/MessagesRestController.java
+++ fila_de_atras/src/main/java/com/filadeatras/fila_de_atras/restcontrollers/MessagesRestController.java
@@ -1,5 +1,7 @@
 package com.filadeatras.fila_de_atras.restcontrollers;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.filadeatras.fila_de_atras.UserComponent;
 import com.filadeatras.fila_de_atras.models.Message;
 import com.filadeatras.fila_de_atras.models.Message.ViewMessage;
+import com.filadeatras.fila_de_atras.models.User;
 import com.filadeatras.fila_de_atras.repositories.MessageRepository;
 import com.filadeatras.fila_de_atras.services.MessageService;
 import com.filadeatras.fila_de_atras.services.UserService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/mensajes")
@@ -23,6 +28,9 @@ public class MessagesRestController {
 	
 	@Autowired
 	private UserService serviceUser;
+	
+	@Autowired
+	private UserComponent userComponent;
 	
 	@JsonView(ViewMessage.class)
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
@@ -49,18 +57,31 @@ public class MessagesRestController {
 		return new ResponseEntity<>(msgDeleted,HttpStatus.OK);
 	}
 	
+	
+	@JsonView(ViewMessage.class)
+	@RequestMapping(value = "/a/{username}", method=RequestMethod.GET)
+	public ResponseEntity<Collection<Message>> getMessagesByUsername(@PathVariable String username){
+		System.out.println("no peto aun");
+		User user = serviceUser.findByusername(username);
+		if(user==null){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		System.out.println("no peto aun3");
+		List<Message> messageConver = serviceMessage.findConversationByUserIdOrderByIdDesc(userComponent.getLoggedUser().getId(), user.getId());
+		return new ResponseEntity<>(messageConver,HttpStatus.OK);
+	}
+
 	/*
 	@JsonView(ViewMessage.class)
-	@RequestMapping(value = "/{username}", method=RequestMethod.GET)
-	public ResponseEntity<Message> deleteMessageById(@PathVariable String username){
+	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Message> delete2MessageById(@PathVariable long id){
 		
-		Message msgDeleted;
+		Message msgDeleted = serviceMessage.getOne(id);
 		if(msgDeleted==null){
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		serviceMessage.delete(msgDeleted);
 		return new ResponseEntity<>(msgDeleted,HttpStatus.OK);
 	}*/
-
 }
 
