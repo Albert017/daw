bf6fd08db765d1df856e94332bfe01245a47fd35
helogargon <helogargon@gmail.com>
2017-05-05 17:58:08 +0200

    incluido ng-bootstrap para cambiar javascript de bootstrap por uno compatible con angular

--- app-front/src/app/app.module.ts
+++ app-front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -19,6 +20,7 @@ import { TrendingPageComponent } from './trending-page/trending-page.component';
     TrendingPageComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
