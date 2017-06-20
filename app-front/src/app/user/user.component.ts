ed318b766e625126bc0d70494478249b611cdc16
ElenaMateos193 <elenam19396@gmail.com>
2017-06-20 21:36:46 +0200

    Intento en vano sobre User

--- app-front/src/app/user/user.component.ts
+++ app-front/src/app/user/user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User} from './user.entity';
+import { Post } from 'app/post/post.entity';
+import { Http } from "@angular/http";
+
+const URL = 'http://localhost:8080/api';
 
 @Component({
   selector: 'app-user',
@@ -7,7 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  private user:User;
+  private posts:Post[]=[];
+
+  getUserPosts(username: String){
+      let url=URL + "/posts/"+ username;
+      this.http.get(url).subscribe(
+        response => {
+          let data = response.json();
+          for (var i = 0; i < data.length; i++) {
+            let post = data[i];
+            this.posts.push(post);
+          }
+      },
+        error => console.error(error)
+      );
+  }
+  constructor(private route: ActivatedRoute, private router: Router, private http: Http) {
+    this.getUserPosts("Sara");
+   }
 
   ngOnInit() {
   }
